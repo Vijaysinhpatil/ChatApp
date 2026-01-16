09ebd879e891b96e18dc7c4b022b7c046050c8ce
vijaysinh Patil <vijaysinhpatil28@gmail.com>
2026-01-16 11:59:23 +0530

    code setup

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoute from './routes/auth.route.js'
+import messageRoute from './routes/message.route.js'
 const app = express();
 dotenv.config({});
 
@@ -8,6 +10,10 @@ dotenv.config({});
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+// Routes
+app.use("/api/auth" , authRoute)
+app.use("/api/messages" , messageRoute)
+app.use("/api/auth" , authRoute)
 // listening
 const PORT = process.env.PORT || 8000;
 app.listen(PORT , () => {
