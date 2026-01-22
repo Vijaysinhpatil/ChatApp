ebe15bbfb9c69cccec581e68a5695e2634164d28
vijaysinh Patil <vijaysinhpatil28@gmail.com>
2026-01-22 14:45:03 +0530

    Welcome Email implemented

--- Server/src/config/utils.js
+++ Server/src/config/utils.js
@@ -1,13 +1,14 @@
 
 import jwt from "jsonwebtoken";
+import { ENV } from "./env.js";
 
 export const generateToken = ( userId , res ) => {
 
-    const { JWT_SECRET } = process.env;
+    const { JWT_SECRET } = ENV;
 
     if(!JWT_SECRET)
     {
-        throw new Error("JWT_SECRET is not configured")
+        throw new Error("JWT_SECRET is not configured") 
     }
 
     //  creating a token for user
@@ -23,7 +24,7 @@ export const generateToken = ( userId , res ) => {
         maxAge : 7 * 24 * 60 * 60 * 1000 , // MS
         httpOnly : true, // prevents XSS attacks : cross-site scripting
         sameSite : "strict", // CSRF attacks
-        secure : process.env.NODE_ENV === "development" ? false : true,
+        secure : ENV.NODE_ENV === "development" ? false : true,
     })
 
     return token
